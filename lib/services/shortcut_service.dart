19b8acc0b2f2c8c8698e246228ab3b4ccf3327d3
duynguyendao <duynguyen20222110@gmail.com>
2026-05-13 03:47:40 +0900

    update: settings toggles, RTC flow and GitHub push workflow

--- lib/services/shortcut_service.dart
+++ lib/services/shortcut_service.dart
@@ -15,6 +15,14 @@ class ShortcutService {
     }
   }
 
+  static Future<bool> openMailApp() async {
+    try {
+      return await launchUrl(Uri.parse('message://'));
+    } catch (e) {
+      return false;
+    }
+  }
+
   static Future<bool> is5GEnabled() async {
     // Can't directly check 5G status from iOS app
     // User needs to check Settings or Shortcut can return value
